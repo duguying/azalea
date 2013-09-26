099ded9cd4cd68f7248b2563c3756d94a4786a50
duguying <duguying2008@gmail.com>
2013-09-26 20:37:59 +0800

    create tcp tester

---
+++ tcper/tcper.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   tcper.c
+ * Author: rex
+ * Tcp Tester
+ * Created on 2013/9/26/, 8:26pm
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hello world!\n");
+    return (EXIT_SUCCESS);
+}
+
