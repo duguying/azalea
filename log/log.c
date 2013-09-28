082fca7a375da6251de74a53f24d03fe2842293d
duguying <duguying2008@gmail.com>
2013-09-28 19:54:15 +0800

    log

--- log/log.c
+++ log/log.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 
-void hello(void){
-	printf("hello world!\n");
+
+/**
+ * create a log
+ * @param file filename
+ * @return log handle
+ */
+int log_create(const char* file){
+	int lh;
+	lh=open(file, O_RDWR|O_CREAT|O_APPEND);
+	if(-1==lh){
+		printf("create log file error!\n");
+	}
+	return lh;
+}
+
+/**
+ * write log
+ * @param string log message
+ * @param lh log handle
+ * @return the byte size have written
+ */
+int log(const char* string, int lh){
+	return write(lh, string, strlen(string));
+}
+
+/**
+ * close log
+ * @param lh log handle
+ * @return if success, return 0
+ */
+int log_close(int lh){
+	return close(lh);
 }
--- log/test.c
+++ log/test.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#ifdef _DEBUG_
+#include "log.c"
+#endif
+
 int main(void){
-	hello();
+	int i,lh;
+	lh=log_create("test.log");
+
+	for(i=0;i<10000;i++){
+		log("hello world, this is a log!\n", lh);
+	}
+	//log_close();
+	
+	for(;1;){sleep();}
 }
