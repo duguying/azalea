05958bf6271a04ceba44dda00467785e9cd4dfd7
duguying <duguying2008@gmail.com>
2013-10-12 17:57:41 +0800

    use ip connect mysql server

--- dbs/mysql/mysql_driver.c
+++ dbs/mysql/mysql_driver.c
@@ -8,7 +8,7 @@ int mydrv_initial(char* link){
 	MYSQL *conn;
 	//create a connection
 	conn = mysql_init(NULL);
-	if (mysql_real_connect(conn, "localhost", "root", "lijun", NULL, 0, NULL, 0) == NULL) {
+	if (mysql_real_connect(conn, "127.0.0.1", "root", "lijun", NULL, 0, NULL, 0) == NULL) {
 	      printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
 	      exit(1);
   	}
---
+++ docs/mysql_driver.md
@@ -0,0 +1,3 @@
+MySQL Driver
+----------
+In cygwin debug environment, because of Non-MysqlServer in the cygwin you should set the link address as ip(such as 127.0.0.1), if set the name local, it is connect by pipe file for the cygwin local MysqlServer.
