9a43e4914eea544b36b84ceb04a3559641655a78
duguying <duguying2008@gmail.com>
2013-10-18 00:10:15 +0800

    format target output directory

---
+++ pool/pool.c
@@ -0,0 +1,12 @@
+/**
+ * This is Connection Pool Model
+ * its a solid library
+ * it will be build as a static library
+ * @author Rex Lee
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int pool_init(void){
+	return 0;
+}
