3250ec9688e593f965ad96b3c00fc40dbe23d039
duguying <duguying2008@gmail.com>
2013-12-08 00:31:56 +0800

    start websocket, and test syn in github and git-osc

---
+++ src/prot/websocket.c
@@ -0,0 +1,21 @@
+/**
+ * Azalea Chatting Server
+ *
+ * Copyright (C) 2013-2015 Rex Lee <duguying2008@gmail.com>
+ *
+ * This program is free and opensource software; 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "websocket.h"
+
+void websocket_handshake(){
+	;
+}
+
+void websocket_close(){
+	;
+}
