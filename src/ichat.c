c1f626c3c5ce5a0222067568607daad91bddf83f
duguying <duguying2008@gmail.com>
2013-12-23 16:51:15 +0800

    combine service model with ichat core

--- src/ichat.c
+++ src/ichat.c
@@ -34,9 +34,6 @@ struct sockaddr_in clientaddr;
 //socket id
 int skt;
 
-//thread connect son socket temparay id
-//int son_skt; //this CAN NOT be global
-
 //pipe in
 int pi[2];
 //pipe out
@@ -133,9 +130,7 @@ void* sock_listen(void *arg){
 		memcpy(pipe_buffer, (char*)(&packed_msg), sizeof(Msg));
 
 		log_printf("sock recv: %s,%u: %s\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port), packed_msg.message);
-		//send buffer into pipe
 		
-		//printf("pipe send: %s", (char*)pipe_buffer);
 		rc = write(pi[1], pipe_buffer, PPB_LEN);
 		
 		if( rc == -1 ){
@@ -228,79 +223,89 @@ int main(int argc,char **argv){
 	int pid;
 	//son socket id
 	int son_skt;
-	
-	log_create("test.log");
-	log_printf("test, this is a log!\n");
-
 	int c;
-    
-	while((c = getopt_long(argc, argv, ":", longopts, NULL)) != -1){
-		switch (c){
-			case 'S':
-				if(0==fork()){//son
-					system("./ichat");
-				}else{
-					abort();
-				};
-				break;
-			case 'P':
-				log_printf("Her name is BX.\n");
-				break;
-			case 'R':
-				l_opt_arg = optarg;
-				log_printf("Our love is %s!\n", l_opt_arg);
-				break;
-		}
-	}
+	log_create("test.log");
+	
+ /////////////////////
+	///The main process ended, and then, the son adopt by init
 
+    int main_pid;
+ 	main_pid=fork();
+ 	if (main_pid<0)
+ 	{
+ 		printf("An error occurred! Service have NOT start!\n");
+ 	}else if (main_pid>0)//in father
+ 	{
+ 		printf("ichat started...\n");
+ 		exit(0);//in father
+ 	}else if(main_pid==0){
+//////////////////////
 
-	//pool initial
-	Pool* pool=pool_init();
-
-	//create pipe
-	pipe(pi);
-	pipe(po);
-
-	//load model in new thread
-	//pthread_create(&mtid, NULL, loadmodel, NULL);
-
-	//start listen
-	socket_listen();
-
-	//create process for message dealing task
-	pid=fork();
-	if(0==pid){//in son process
-		close(pi[1]);//close send, use recv
-		close(po[0]);//close recv, use send
-		log_printf("son start\n");
-		char pipe_buffer[PPB_LEN];
-		int rc;
-		while((rc = read(pi[0], pipe_buffer, PPB_LEN)) > 0){
-			log_printf("son pipe recv: %s, from %d\n",((Msg*)pipe_buffer)->message,((Msg*)pipe_buffer)->from);
-
-			//pipe_buffer[PPB_LEN]=13;
-			//memcpy(pipe_buffer, (char*)(&packed_msg), sizeof(Msg));
-
-			rc = write(po[1], pipe_buffer, PPB_LEN);	//write the recvd msg into po for sendding to sock sender
-			memset(pipe_buffer, ECF, PPB_LEN);
-		}
-	}else{// in fahter
-		close(pi[0]);//close recv, use send
-		close(po[1]);//close send, use recv
-		//create new thread to manage pipe listen
-		err = pthread_create(&ptid, NULL, pipe_listen, NULL);
-		//sock length
-		len = sizeof(clientaddr);
-		//loop listen and accept
-		for(;1;){
-			//The function `accept` can block the process, so, i need't sleep
-			if(son_skt=accept(skt, (struct sockaddr*)&clientaddr, &len)){
-				log_printf("\033[1;32m%s,%u;skt %d: Connected!\033[1;0m\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), son_skt);
-				//if accepted, create thread!
-				err = pthread_create(&ntid, NULL, sock_listen, &son_skt);//send the son socket
+		while((c = getopt_long(argc, argv, ":", longopts, NULL)) != -1){
+			switch (c){
+				case 'S':
+					if(0==fork()){//son
+						system("./ichat");
+					}else{
+						abort();
+					};
+					break;
+				case 'P':
+					log_printf("Her name is BX.\n");
+					break;
+				case 'R':
+					l_opt_arg = optarg;
+					log_printf("Our love is %s!\n", l_opt_arg);
+					break;
 			}
 		}
-	}
 
-	return 0;
+
+		//pool initial
+		Pool* pool=pool_init();
+
+		//create pipe
+		pipe(pi);
+		pipe(po);
+
+		//start listen
+		socket_listen();
+
+		//create process for message dealing task
+		pid=fork();
+		if(0==pid){//in son process
+			close(pi[1]);//close send, use recv
+			close(po[0]);//close recv, use send
+			log_printf("son start\n");
+			char pipe_buffer[PPB_LEN];
+			int rc;
+			while((rc = read(pi[0], pipe_buffer, PPB_LEN)) > 0){
+				log_printf("son pipe recv: %s, from %d\n",((Msg*)pipe_buffer)->message,((Msg*)pipe_buffer)->from);
+
+				rc = write(po[1], pipe_buffer, PPB_LEN);	//write the recvd msg into po for sendding to sock sender
+				memset(pipe_buffer, ECF, PPB_LEN);
+			}
+		}else{// in fahter
+			close(pi[0]);//close recv, use send
+			close(po[1]);//close send, use recv
+			//create new thread to manage pipe listen
+			err = pthread_create(&ptid, NULL, pipe_listen, NULL);
+			//sock length
+			len = sizeof(clientaddr);
+			//loop listen and accept
+			for(;1;){
+				//The function `accept` can block the process, so, i need't sleep
+				if(son_skt=accept(skt, (struct sockaddr*)&clientaddr, &len)){
+					log_printf("\033[1;32m%s,%u;skt %d: Connected!\033[1;0m\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), son_skt);
+					//if accepted, create thread!
+					err = pthread_create(&ntid, NULL, sock_listen, &son_skt);//send the son socket
+				}
+			}
+		}
+
+///////////////////////
+	}
+ 	return 0;
+///////////////////////
+
 }
