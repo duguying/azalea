1eb11ac738d7c4f26f77fcf956011f8e4848774f
duguying <duguying2008@gmail.com>
2013-12-25 00:36:42 +0800

    add installation into cmakelists.txt

--- tests/test_hashtable.c
+++ tests/test_hashtable.c
@@ -15,20 +15,13 @@ int main(void){
 	HashTable hash_table;
 	HashNode* lookup_result;
 	int i;
+	ht_init(&hash_table);
 	for (i = 0; i < 1000; ++i)
 	{
-		ht_init(&hash_table);
-		ht_insert(&hash_table,"rex", 123);
-		ht_insert(&hash_table,"lee", 123);
-		ht_insert(&hash_table,"lijun", 12346);
-		hash_table_print(&hash_table);
-		ht_remove(&hash_table,"rex");
-		hash_table_print(&hash_table);
-		lookup_result=ht_lookup(&hash_table,"lijun");
-		printf("the key:%s  the value:%d\n", lookup_result->sKey, lookup_result->nValue);
-		printf("\n%s\n", "After release");
-		ht_release(&hash_table);
-		hash_table_print(&hash_table);
+		ht_insert(&hash_table,"i", i);	
 	}
+	hash_table_print(&hash_table);
+	ht_release(&hash_table);
+	hash_table_print(&hash_table);
 	return 0;
 }
