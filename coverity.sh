d8b4b9447a2afa8f42f5d41daaf6103f186bb66f
duguying <duguying2008@gmail.com>
2014-02-27 19:00:51 +0800

    coverity

--- coverity.sh
+++ coverity.sh
@@ -5,7 +5,7 @@ case `uname -m` in
 	amd64|x86_64)	BITS=64 ;;
 esac
 
-wget https://scan.coverity.com/download/linux-${BITS} -O cov-build-tools.gz --post-data "project=duguying/Azalea&token=FgaZPLkFHxZOZXrOFUJm6Q"
+wget https://scan.coverity.com/download/linux-${BITS} -O cov-build-tools.gz --post-data "project=duguying/Azalea&token=$COVERITY_SCAN_TOKEN"
 tar -zxvf cov-build-tools.gz
 
 COV_DIR=`find ./ -type d -name 'cov-analysis*'`
@@ -14,7 +14,7 @@ export PATH=$PATH:$COV_DIR/bin
 cov-build --dir cov-int make
 tar czvf azalea.tgz cov-int
 curl --form project=duguying/Azalea \
-  --form token=FgaZPLkFHxZOZXrOFUJm6Q \
+  --form token=$COVERITY_SCAN_TOKEN \
   --form email=duguying2008@gmail.com \
   --form file=@azalea.tgz \
   --form version=Version \
