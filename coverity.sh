91a9cf7c8622a93da3a7428efe88b66d3518127b
duguying <duguying2008@gmail.com>
2014-03-03 10:25:14 +0800

    fix coverity.sh

--- coverity.sh
+++ coverity.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+mkdir build
+cd build
+cmake -D CI=true ..
+
 case `uname -m` in
 	i?86)				BITS=32 ;;
 	amd64|x86_64)	BITS=64 ;;
 esac
 
 wget https://scan.coverity.com/download/linux-${BITS} -O cov-build-tools.tar.gz --post-data "project=duguying/Azalea&token=$COVERITY_SCAN_TOKEN"
-tar -zxf cov-build-tools.gz
+tar -zxf cov-build-tools.tar.gz
 
 COV_DIR=`find ./ -type d -name 'cov-analysis*'`
 export PATH=$PATH:$COV_DIR/bin
@@ -20,3 +24,5 @@ curl --form project=duguying/Azalea \
   --form version=Version \
   --form description=Description \
   http://scan5.coverity.com/cgi-bin/upload.py
+
+make test
