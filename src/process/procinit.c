d2973bef82ff111c0822eb532e8bf7fc20333b7c
Rex Lee <duguying2008@gmail.com>
2014-04-29 08:41:03 +0800

    process initial model, create master and worker process

---
+++ src/process/procinit.c
@@ -0,0 +1,27 @@
+/**
+ * Azalea Chatting Server
+ *
+ * Copyright (C) 2013-2015 Rex Lee <duguying2008@gmail.com>
+ *
+ * This program is free and opensource software; 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License
+ */
+
+#include "procinit.h"
+
+int proc_init(){
+	return 0;
+}
+
+int proc_create_sock_serv(){
+	return 0;
+}
+
+int proc_create_sock_client(){
+	return 0;
+}
+
+int proc_try_connect_serv(){
+	return 0;
+}
---
+++ src/process/procinit.h
@@ -0,0 +1,19 @@
+/**
+ * Azalea Chatting Server
+ *
+ * Copyright (C) 2013-2015 Rex Lee <duguying2008@gmail.com>
+ *
+ * This program is free and opensource software; 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License
+ */
+
+#include "ichat.h"
+
+int proc_init();
+
+int proc_create_sock_serv();
+
+int proc_create_sock_client();
+
+int proc_try_connect_serv();
